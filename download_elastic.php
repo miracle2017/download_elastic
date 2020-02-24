45b468ddfdd4e6b53da8e2e25be638020691d0f2
miracle <610010559@qq.com>
2020-02-24 10:17:13 +0800

    good

---
+++ download_elastic.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/2/21
+ * Time: 13:18
+ */
+
+echo PHP_EOL . '------ start at ' . date('Y-m-d H:i:s') . '------' . PHP_EOL;
+$url = 'https://www.elastic.co/cn/downloads/past-releases';
+$baseReg = '#"url"[^"]*"(http[^"]*?%s-((\d*|\.)*)-%s-[^"]*?)"#';
+
+$products = ['elasticsearch', 'kibana'];
+$OS = ['windows', 'linux', 'darwin'];
+
+if ($versionLogJson = file_get_contents('./version.log')) {
+    try {
+        $version = json_decode($versionLogJson, true);
+    } catch (Exception $e) {
+        var_dump($e);
+        die('json_decode fail');
+    }
+} else {
+    var_dump(error_get_last());
+    die("get version.log fail");
+}
+
+//$page = `curl $url`;
+$page = file_get_contents('./t.html');
+$updateVersion = [];
+foreach ($products as $k => $product_name) {
+    foreach ($OS as $os_key => $os_name) {
+        echo "$product_name-$os_name:";
+        $reg = sprintf($baseReg, $product_name, $os_name);
+        $re = preg_match_all($reg, $page, $m);
+        $currentVersion = $version[$product_name][$os_name] ?? "7.6.0";
+        if (false !== ($key = array_search($currentVersion, $m[2]))) {
+            if (empty($downloadList = array_splice($m[1], 0, $key))) {
+                die("current version is Newest");
+            }
+            foreach ($downloadList as $download_key => $url) {
+                `cd d && wget $url`;
+            }
+            $updateVersion[$product_name][$os_name] = (string)$m[2][0];
+            var_dump($downloadList);
+        } else {
+            var_dump('current version is not in pages');
+        }
+        echo "-----" . PHP_EOL;
+    }
+}
+
+$saveVersion = json_encode($updateVersion, JSON_UNESCAPED_UNICODE);
+file_put_contents('./version.log', $saveVersion);
+die(PHP_EOL . "--- the end ---" . PHP_EOL);
