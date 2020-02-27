76b1a9b252f1b9f9d48ac4c0d9f5cb24c1091920
miracle <610010559@qq.com>
2020-02-28 01:15:48 +0800

    good

--- download.php
+++ download.php
@@ -13,14 +13,30 @@ $limit = array(
     'maxVersion' => '7.3.2',
     'minVersion' => '7.0.0',
 );
-$i = 0;
+
 foreach ($products as $pk => $pv) {
+    $m[$pv] = [];
     foreach ($OS as $ok => $ov) {
-        foreach ($list[$pv][$ov] as $v => $url) {
-            if (strcmp($v, $limit['maxVersion']) <= 0 && strcmp($v, $limit['minVersion']) >= 0) {
-                echo "$pv-$ov-$v:$url" . PHP_EOL;
-            `cd d && wget --user-agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'  $url`;
-            }
+        $m[$pv] = array_merge_recursive($m[$pv], $list[$pv][$ov]);
+//        foreach ($list[$pv][$ov] as $v => $url) {
+//            if (strcmp($v, $limit['maxVersion']) <= 0 && strcmp($v, $limit['minVersion']) >= 0) {
+//                echo "$pv-$ov-$v:$url" . PHP_EOL;
+//            }
+//        }
+    }
+}
+$merge = [];
+foreach ($m as $p => $pv) {
+    $merge = array_merge_recursive($merge, $pv);
+}
+
+foreach ($merge as $version => $value) {
+    if (strcmp($version, $limit['maxVersion']) <= 0 && strcmp($version, $limit['minVersion']) >= 0) {
+        echo "$version:" . PHP_EOL;
+        foreach ($value as $url) {
+            echo "|--$url" . PHP_EOL;
+//            `cd d && wget --user-agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'  $url`;
+            $i++;
         }
     }
 }
